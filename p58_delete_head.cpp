01bb48ab44e8931499bf869c5fd82363c7f98252
Nafiz Himel <nafizhimel7@gmail.com>
2026-01-10 20:48:41 +0600

    new files added

---
+++ p58_delete_head.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void input(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+    tail->next = newnode;
+    tail = newnode;
+}
+
+void delete_head(Node *&head)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+    Node *tmp = head;
+    head = head->next;
+    delete tmp;
+}
+
+void print(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << endl;
+        tmp = tmp->next;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        input(head, tail, val);
+    }
+    delete_head(head);
+    print(head);
+    return 0;
+}
